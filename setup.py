d4fe12eab48ba60e678a77c3c1f4eed941b7603b
Paul Müller <dev@craban.de>
2021-01-22 16:10:16 +0100

    ci: migrate to gh actions and use install_requires

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ name = 'ckanext-dcor_theme'
 year = "2020"
 
 sys.path.insert(0, realpath(dirname(__file__))+"/" + "/".join(name.split("-")))
-from _version import version
+from _version import version  # noqa: E402
 
 
 setup(
@@ -27,10 +27,8 @@ setup(
     package_dir={name: name},
     namespace_packages=['ckanext'],
     install_requires=[
-        # CKAN extensions should not list dependencies here, but in a separate
-        # ``requirements.txt`` file.
-        #
-        # http://docs.ckan.org/en/latest/extensions/best-practices.html#add-third-party-libraries-to-requirements-txt
+        # the "ckan" dependency is implied
+        "Babel>=2.8.0",
     ],
     include_package_data=True,
     # To provide executable scripts, use entry points in preference to the

4fe41d660fee0825327099586b8a3dc56bc34633
Paul Müller <dev@craban.de>
2021-04-15 22:51:06 +0200

    enh: add apple-touch-icon images

---
+++ ckanext/dcor_theme/public/generate_icons.sh
@@ -0,0 +1,8 @@
+inkscape favicon.svg -e favicon.png -w 32
+icotool -c favicon.ico favicon.png
+mkdir -p images
+inkscape favicon.svg -e images/dcor-076.png -w 76
+inkscape favicon.svg -e images/dcor-096.png -w 96
+inkscape favicon.svg -e images/dcor-152.png -w 152
+inkscape favicon.svg -e images/dcor-180.png -w 152
+

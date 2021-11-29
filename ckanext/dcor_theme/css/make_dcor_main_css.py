8e92c016e72463316f914b962fc164bce55d6746
Paul Müller <dev@craban.de>
2021-11-29 01:31:52 +0100

    finally make things work with #2 and https://github.com/ckan/ckan/issues/6563

--- ckanext/dcor_theme/css/make_dcor_main_css.py
+++ ckanext/dcor_theme/css/make_dcor_main_css.py
@@ -2,7 +2,8 @@
 import pathlib
 
 #: Path to CKAN's main.css
-ckan_css_path = "/usr/lib/ckan/default/src/ckan/ckan/public/base/css/main.css"
+ckan_css_path = pathlib.Path(
+    "/usr/lib/ckan/default/src/ckan/ckan/public/base/css/main.css")
 
 #: Dictionary mapping default colors to DCOR colors
 color_map = {
@@ -11,7 +12,6 @@ color_map = {
     "#005d7a": "#163868",
     "#206b82": "#2C55A4",  # btn-primary background and link
     "#1b5a6e": "#1f4d8d",  # btn-primary border
-    "#164959": "#194493",  # btn-primary focus background
     "#020607": "#1f4d8d",  # btn-primary focus border
     "#164959": "#194493",  # btn-primary hover background
     "#0f323c": "#1f4d8d",  # btn-primary hover border
@@ -24,25 +24,37 @@ color_map = {
 
 
 def main():
+    # Notes:
+    # - We have to edit webassets.yml: https://github.com/ckan/ckan/issues/6563
+    # - We cannot use our own location, we have to use the same.
+
+    # https://github.com/ckan/ckan/issues/6563
+    webassets_path = ckan_css_path.with_name("webassets.yml")
+    webassets_text = webassets_path.read_text()
+    if not webassets_text.count("dcor_main"):
+        webassets_text += "dcor_main:\n" \
+                          + "  filters: cssrewrite\n" \
+                          + "  output: base/%(version)s_dcor_main.css\n" \
+                          + "  contents: dcor_main.css\n"
+    webassets_path.write_text(webassets_text)
+
+    dcor_css_path = ckan_css_path.with_name("dcor_main.css")
     here = pathlib.Path(__file__).parent
-    dcor_css_path = here / "dcor_main.css"
 
     # replace colors
-    main_css_data = pathlib.Path(ckan_css_path).read_text()
-    for (old, new) in color_map:
-        main_css_data = main_css_data.replace(old, new)
+    main_css_data = ckan_css_path.read_text()
+    for color in color_map:
+        main_css_data = main_css_data.replace(color, color_map[color])
 
     # add custom css inputs
-    css_inputs = sorted(here.glob("*.css"))
-    css_inputs = [pp for pp in css_inputs if not pp.samefile(dcor_css_path)]
-    for ci in css_inputs:
+    for ci in sorted(here.glob("*.css")):
         main_css_data += "\n"
         main_css_data += ci.read_text()
 
     dcor_css_path.write_text(main_css_data)
 
-    print(
-        f"Make sure to set 'ckan.main_css = {dcor_css_path}' in CKAN config!")
+    print("Make sure to set 'ckan.main_css=/base/css/dcor_main.css' "
+          + "in the CKAN ini config!")
 
 
 if __name__ == "__main__":

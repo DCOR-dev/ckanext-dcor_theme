78a77cd7e89ae9ae636423f11139f6f78afa74af
Paul Müller <dev@craban.de>
2024-02-26 20:57:58 +0100

    tests: improve coverage

--- ckanext/dcor_theme/tests/test_plugin.py
+++ ckanext/dcor_theme/tests/test_plugin.py
@@ -2,5 +2,21 @@
 import ckanext.dcor_theme.plugin as plugin  # noqa: F401
 
 
-def test_plugin():
-    pass
+class BlueprintChecker:
+    def add_url_rule(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+
+def test_plugin_get_blueprint():
+    p = plugin.DCORThemePlugin()
+    blueprint = p.get_blueprint()
+    funcs = blueprint.deferred_functions
+    endpoints = []
+    for func in funcs:
+        checker = BlueprintChecker()
+        func(checker)
+        endpoints.append(checker.args[1])
+    assert "contact" in endpoints
+    assert "imprint" in endpoints
+    assert "privacy" in endpoints

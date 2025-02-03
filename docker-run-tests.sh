bf70787751bd075e4d1592c19827e7cd5cb1b43d
RaghavaAlajangi <raghava.alajangi95@gmail.com>
2025-02-03 17:06:09 +0100

    fix: configure Ci piepline properly

--- docker-run-tests.sh
+++ docker-run-tests.sh
@@ -3,17 +3,14 @@
 # Fail immediately if any command fails
 set -e
 
-CKAN_CONTAINER="dcor-ckan-docker-ckan"
 EXTENSION_NAME="ckanext-dcor_theme"  # Change this to your extension's name
+CKAN_CONTAINER="${EXTENSION_NAME}-dcor-test-1"
 EXTENSION_PATH="/srv/app/src_extensions/${EXTENSION_NAME}"
-
-echo "Copying extension to the container..."
-docker cp . ${CKAN_CONTAINER}:${EXTENSION_PATH}
 
 echo "Creating a virtual environment inside the CKAN container..."
 docker exec -it ${CKAN_CONTAINER} /bin/bash -c "
   cd ${EXTENSION_PATH};
-  python3 -m venv venv;
+  python3 -m venv --system-site-packages venv;
   source venv/bin/activate;
   pip install --upgrade pip wheel;
   dcor update --yes;
@@ -25,7 +22,8 @@ echo "Running tests in the virtual environment..."
 docker exec -it ${CKAN_CONTAINER} /bin/bash -c "
   cd ${EXTENSION_PATH};
   source venv/bin/activate;
-  pytest --cov=/ckanext.dcor_theme --cov-omit="*tests*" -p no:warnings --cov-report=xml;
+  coverage run --source=ckanext.dcor_theme --omit=*tests* -m pytest -p no:warnings ckanext;
+  coverage xml
 "
 
 echo "Tests completed!"

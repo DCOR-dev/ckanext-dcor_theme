27893c1a4daa565ef32a5336ab07512b68648da5
RaghavaAlajangi <raghava.alajangi95@gmail.com>
2025-02-18 17:17:08 +0100

    fix: update docker configuration to avoid permission errors and run tests as ckan user

--- docker-run-tests.sh
+++ docker-run-tests.sh
@@ -4,10 +4,10 @@
 set -e
 
 EXTENSION_NAME="ckanext-dcor_theme"  # Change this to your extension's name
-CKAN_CONTAINER="${EXTENSION_NAME}-dcor-test-1"
+CKAN_CONTAINER="${EXTENSION_NAME}-dcor-test-1" # ckan ontainer name
 EXTENSION_PATH="/srv/app/src_extensions"
 
-echo "Creating a virtual environment inside the CKAN container..."
+# Create venv and install dependencies as a root user inside the container
 docker exec -u root ${CKAN_CONTAINER} bash -c "
   cd ${EXTENSION_PATH};
   python3 -m venv --system-site-packages venv;
@@ -18,8 +18,9 @@ docker exec -u root ${CKAN_CONTAINER} bash -c "
   pip install -r ./ckanext/dcor_theme/tests/requirements.txt;
 "
 
+# Run tests as a ckan user
 echo "Running tests in the virtual environment..."
-docker exec ${CKAN_CONTAINER} bash -c "
+docker exec -u ckan ${CKAN_CONTAINER} bash -c "
   cd ${EXTENSION_PATH};
   source venv/bin/activate;
   coverage run --source=ckanext.dcor_theme --omit=*tests* -m pytest -p no:warnings ckanext;

7fa2ef196ccf7301860ed8eff2537e1c2c04011a
Paul Müller <dev@craban.de>
2025-07-29 23:24:52 +0200

    reg: do not change layout, add line-breaks instead

--- ckanext/dcor_theme/cli/patch_ckan_templates.py
+++ ckanext/dcor_theme/cli/patch_ckan_templates.py
@@ -22,20 +22,18 @@ def patch_wider_resource_listing():
     """Make sure the full filename is visible in the resource view"""
     ckan_path = pathlib.Path(ckan.__file__).parent
 
-    # patch page to increase resource file listing width
-    path_template_page = ckan_path / "templates/page.html"
-    data_page = path_template_page.read_text()
-    for old, new in [
-        ('<aside class="secondary col-md-3">',
-         '<aside class="secondary col-md-5">'),
-        ('<div class="primary col-md-9 col-xs-12" role="main">',
-         '<div class="primary col-md-7 col-xs-12" role="main">')
-    ]:
-        data_page = data_page.replace(old, new)
-    path_template_page.write_text(data_page)
-
-    # patch resource listing template, removing the truncate filter
+    # patch resource listing template
     path_template_res = ckan_path / "templates/package/snippets/resources.html"
     data_res = path_template_res.read_text()
-    data_res = data_res.replace("|truncate(25)", "")
+    st = 'style="word-wrap: break-word"'
+    for old, new in [
+        # remove the truncate filter for file names
+        ("|truncate(25)", ""),
+        # force word wrap of the file names
+        ('<a href="',
+         f'<a {st} href="'),
+        ('<a class="flex-fill" href="',
+         f'<a class="flex-fill" {st} href="'),
+    ]:
+        data_res = data_res.replace(old, new)
     path_template_res.write_text(data_res)

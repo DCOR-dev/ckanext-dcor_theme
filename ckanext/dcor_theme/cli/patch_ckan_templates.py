d87093f52e13fefc88deddcd5665e8bcd54064cc
Paul Müller <dev@craban.de>
2025-07-29 23:29:52 +0200

    docs: comment code

--- ckanext/dcor_theme/cli/patch_ckan_templates.py
+++ ckanext/dcor_theme/cli/patch_ckan_templates.py
@@ -25,6 +25,8 @@ def patch_wider_resource_listing():
     # patch resource listing template
     path_template_res = ckan_path / "templates/package/snippets/resources.html"
     data_res = path_template_res.read_text()
+    # This makes the browser force a line-break even inside a word,
+    # which means that it breaks long filenames.
     st = 'style="word-wrap: break-word"'
     for old, new in [
         # remove the truncate filter for file names

a47243367ea1097bdf3c46801928f79af8089ec5
Paul Müller <dev@craban.de>
2025-07-30 11:39:42 +0200

    enh: also patch resource_item listing for dataset view

--- ckanext/dcor_theme/cli/patch_ckan_templates.py
+++ ckanext/dcor_theme/cli/patch_ckan_templates.py
@@ -22,7 +22,24 @@ def patch_wider_resource_listing():
     """Make sure the full filename is visible in the resource view"""
     ckan_path = pathlib.Path(ckan.__file__).parent
 
-    # patch resource listing template
+    # patch resource listing package template
+    path_template_res_item = \
+        ckan_path / "templates/package/snippets/resource_item.html"
+    data_res_item = path_template_res_item.read_text()
+    # This makes the browser force a line-break even inside a word,
+    # which means that it breaks long filenames.
+    st = 'style="word-wrap: break-word"'
+    for old, new in [
+        # remove the truncate filter for file names
+        ("| truncate(50)", ""),
+        # force word wrap of the file names
+        ('<a class="heading"',
+         f'<a {st} class="heading"'),
+    ]:
+        data_res_item = data_res_item.replace(old, new)
+    path_template_res_item.write_text(data_res_item)
+
+    # patch resource listing details template
     path_template_res = ckan_path / "templates/package/snippets/resources.html"
     data_res = path_template_res.read_text()
     # This makes the browser force a line-break even inside a word,
